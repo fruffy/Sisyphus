0a28b46db8df5feeab32c7a108a32e17e8cbfec8
Joey Eremondi <joey.eremondi@gmail.com>
2017-02-03 15:17:55 -0800

    Method variable renaming for var normalizer

--- 507/src/normalizers/VariableNameNormalizer.java
+++ 507/src/normalizers/VariableNameNormalizer.java
@@ -3,12 +3,17 @@ package normalizers;
 import java.util.Collection;
 import java.util.LinkedList;
 
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.AnnotationExpr;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.type.ReferenceType;
 import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.ast.visitor.Visitable;
@@ -35,7 +40,7 @@ public class VariableNameNormalizer extends Normalizer {
 		public VariableEnv gamma;
 		public Collection<Pair<String, String>> declsForParent;
 
-		public VisitInfo(VariableEnv argGamma, LinkedList<Pair<String, String>> decls){
+		public VisitInfo(VariableEnv argGamma, Collection<Pair<String, String>> decls){
 			gamma = argGamma;
 			declsForParent = decls;
 		}
@@ -91,29 +96,69 @@ public class VariableNameNormalizer extends Normalizer {
 		@Override
 		public Visitable visit(final BlockStmt n, final VisitInfo info) {
 			//visitComment(n, info);
-			
+
 			//The environment of variables declared after each statement
 			VariableEnv gammaCurrent = info.gamma;
-			
+
 			//Where we'll store the modified statements from our list after we process them
 			NodeList<Statement> newStatementList = new NodeList<Statement>();
-			
+
 			for (Statement stmt : n.getStatements()){
 				//Make the map where we store declarations from the statement, so we can use them later on
 				LinkedList<Pair<String, String>> childDecls = new LinkedList<Pair<String, String>>();
-				
+
 				//Process the statement, receiving its declarations in a new list
 				Statement newStmt = (Statement) stmt.accept(this, new VisitInfo(gammaCurrent, childDecls) );
-				
+
 				//Add all the declarations from inside that statement to our environemnt for future statements
 				gammaCurrent = gammaCurrent.appendFront(childDecls);
 				newStatementList.add(newStmt);
 			}
-			
+
 			n.setStatements(newStatementList);
 			return n;
 		}
-		
+
+		//We need to add the variables from a method declaration into our scope
+		@Override
+		public Visitable visit(final MethodDeclaration n, final VisitInfo info) {
+			//visitComment(n, info);
+			n.setAnnotations((NodeList<AnnotationExpr>) n.getAnnotations().accept(this, info));
+			n.setTypeParameters(modifyList(n.getTypeParameters(), info));
+			n.setType((Type) n.getType().accept(this, info));
+			
+			
+			//We process parameters manually so we can rename them
+			//and store their new names, to use when processing the body
+			NodeList<Parameter> newParams = new NodeList<Parameter>();
+			LinkedList<Pair<String, String>> childDecls = new LinkedList<Pair<String, String>>();
+			for (Parameter param : n.getParameters()){
+				Parameter newParam = (Parameter)param.accept(this, new VisitInfo(info.gamma, childDecls));
+				newParams.add(newParam);
+			}
+			n.setParameters(newParams);
+			
+			
+			n.setThrownExceptions((NodeList<ReferenceType>) n.getThrownExceptions().accept(this, info));
+			
+			//Now, when processing the body, use an extended environment with the parameters' new names
+			VariableEnv newGamma = info.gamma.appendFront(childDecls);
+			if (n.getBody().isPresent()) {
+				n.setBody((BlockStmt) n.getBody().get().accept(this, new VisitInfo(newGamma, info.declsForParent)));
+			}
+			return n;
+		}
+
+		//Helpers copied from ModifyVisitor, they should really be protected
+		private <N extends Node> NodeList<N> modifyList(NodeList<N> list, VisitInfo arg) {
+			if (list == null) {
+				return null;
+			}
+			return (NodeList<N>) list.accept(this, arg);
+		}
+
+
+
 		//TODO: find all places where variables are defined
 		//So that we can carry their decls forwards
 		//i.e. for loops, functions, catch-blocks
