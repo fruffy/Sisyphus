71cf5cb992f5af921cf1632b95523d9f4c80aad3
Joey Eremondi <joey.eremondi@gmail.com>
2017-02-23 13:20:09 -0800

    Use new Node format, fill in missing parts

--- 507/src/dfg/ReachingDefsFinder.java
+++ 507/src/dfg/ReachingDefsFinder.java
@@ -11,123 +11,168 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.expr.AssignExpr;
 import com.github.javaparser.utils.Pair;
 
-import cfg.CFGNode;
+import jgrapht.DirectedGraph;
+import jgrapht.experimental.dag.DirectedAcyclicGraph;
+import jgrapht.graph.DefaultEdge;
 
+import visitors.ASTUtil;
+
+/**
+ * Based on "Reaching Definitions" from Principles of Program Analysis
+ * by Nielson, Nielson and Hankin
+ * pp 43-44
+ *
+ */
 public class ReachingDefsFinder {
-	
+
 	private class Worklist{
-		
-		private LinkedHashSet<CFGNode> innerSet;
+
+		private LinkedHashSet<Node> innerSet;
 
 		public boolean isEmpty(){
 			return innerSet.isEmpty();
 		}
-		
-		public void push(CFGNode elem){
+
+		public void push(Node elem){
 			innerSet.add(elem);
 		}
-		
-		public void push(Collection<CFGNode> coll){
+
+		public void push(Collection<Node> coll){
 			innerSet.addAll(coll);
 		}
-		
-		public CFGNode pop(){
-			Iterator<CFGNode> iterator = innerSet.iterator();
-			CFGNode result = iterator.next();
+
+		public Node pop(){
+			Iterator<Node> iterator = innerSet.iterator();
+			Node result = iterator.next();
 			iterator.remove();
 			return result;
 		}
-		
+
 		public Worklist(){
-			this.innerSet = new LinkedHashSet<CFGNode>();
+			this.innerSet = new LinkedHashSet<Node>();
 		}
-		
+
 	}
-	
-	private final Set<CFGNode> allNodes;
+
+	private final DirectedAcyclicGraph<Node, DefaultEdge> cfg;
+	private final Set<Node> allNodes;
 	private Worklist worklist;
-	
-	private HashMap<CFGNode, Set<Pair<String, AssignExpr>>> exitSet;
-	private HashMap<CFGNode, Set<Pair<String, AssignExpr>>> entrySet;
-	
+
+	private HashMap<Node, Set<Pair<String, AssignExpr>>> exitSet;
+	private HashMap<Node, Set<Pair<String, AssignExpr>>> entrySet;
+
 	static Set<Pair<String, AssignExpr>> bottom(){
 		return new HashSet<Pair<String, AssignExpr>>();
 	}
-	
-	//TODO move this into CFG package?
-	static Set<Pair<String, AssignExpr>> killSet(CFGNode node){
-		return null;
+
+	private boolean inKillSet(Pair<String, AssignExpr> defPair, Node node){
+		//Assignments kill definitions of the variables they assign to
+		if (node instanceof AssignExpr){
+			AssignExpr aexpr = (AssignExpr)node;
+			//Check if the think we possibly kill is 
+			//an assignment to the variable we are assigning
+			if (aexpr.getTarget().toString() == defPair.a){
+				return true;
+			}
+		}
+
+		//Only assignments kill
+		//TODO other things that kill?
+		return false;
 	}
-	
-	static Set<Pair<String, AssignExpr>> genSet(CFGNode node){
-		return null;
+
+	static Set<Pair<String, AssignExpr>> genSet(Node node){
+		HashSet<Pair<String, AssignExpr>> ret = new HashSet<Pair<String, AssignExpr>>();
+
+		//Assignments generate definitions of the variables they assign to
+		if (node instanceof AssignExpr){
+			AssignExpr aexpr = (AssignExpr)node;
+			//Check if the think we possibly kill is 
+			//an assignment to the variable we are assigning
+			new Pair<String, AssignExpr>(aexpr.getTarget().toString(), aexpr);
+		}
+		
+		//TODO other things that gen? Method calls?
+		
+		return ret;
 	}
-	
-	
-	public ReachingDefsFinder(Set<CFGNode> allNodes){
-		this.allNodes = allNodes;
+
+
+	public ReachingDefsFinder(DirectedAcyclicGraph<Node, DefaultEdge> cfg){
+		this.cfg = cfg;
+		this.allNodes = cfg.vertexSet();
 		this.worklist = new Worklist();
 	}
-	
-	public Map<CFGNode, Set<Pair<String, AssignExpr>>> findReachingDefs(){
+
+	public DirectedGraph<Node, DefaultEdge> findReachingDefs(){
 		//Set the reaching defs to bottom (empty set) for each node
-		for (CFGNode node : allNodes){
+		for (Node node : allNodes){
 			entrySet.put(node, bottom());
 			exitSet.put(node, bottom());
 		}
-		
-		
+
+
 		//Add all nodes to our worklist initially, since we need to update values for each node
 		worklist.push(allNodes);
-		
+
 		//Keep updating by the dataflow equations until nothing is on our worklist
 		while (!worklist.isEmpty()){
-			CFGNode currentNode = worklist.pop();
+			Node currentNode = worklist.pop();
 			Set<Pair<String, AssignExpr>> currentEntry = entrySet.get(currentNode);
 			Set<Pair<String, AssignExpr>> currentExit = exitSet.get(currentNode);
-			
+
 			Set<Pair<String, AssignExpr>> newEntry = entrySet.get(currentNode);
 			Set<Pair<String, AssignExpr>> newExit = exitSet.get(currentNode);
-			
+
 			if (false){ //TODO check if is initial label
 				//TODO set to free vars of graph
 			}
 			else {
-				//Our entry is our predecessor's exit
-				for (CFGNode predNode : currentNode.getPredecessors()){
-					newEntry.addAll(exitSet.get(predNode));
+				//Our entry contains all of our predecessors' exits
+				for (DefaultEdge incomingEdge : cfg.incomingEdgesOf(currentNode) ){
+					Node previousNode = cfg.getEdgeSource(incomingEdge);
+					newEntry.addAll(exitSet.get(previousNode));
 				}
-				
+
 				//Our exit set: add everything from entry that wasn't killed
 				//Plus anything new we defined in this statement
-				Set<Pair<String, AssignExpr>> killSet = killSet(currentNode);
 				for (Pair<String, AssignExpr> pair : newEntry){
-					if (!killSet.contains(pair)){
+					if (!inKillSet(pair, currentNode)){
 						newExit.add(pair);
 					}
 				}
 				newExit.addAll(genSet(currentNode));
 			}
-			
+
 			//Update our new entry and exit values
 			entrySet.put(currentNode, newEntry);
 			exitSet.put(currentNode, newExit);
-			
+
 			//Add all our successors to the worklist
-			//TODO faster successor check?
-			for (CFGNode maybeSuccessor : allNodes){
-				if (maybeSuccessor.getPredecessors().contains(currentNode)){
-					worklist.push(maybeSuccessor);
+			Set<DefaultEdge> outgoingEdges = cfg.outgoingEdgesOf(currentNode);
+			for (DefaultEdge edge : outgoingEdges){
+				worklist.push(cfg.getEdgeTarget(edge));
+			}
+		}
+
+		DirectedGraph<Node, DefaultEdge> ret =
+				new DirectedAcyclicGraph<Node, DefaultEdge>(DefaultEdge.class);
+
+		for (Node n : allNodes){
+			for (Pair<String, AssignExpr> defPair : exitSet.get(n)){
+				//If a def reaches a node, and that node references that variable
+				//then we have an edge
+				//TODO check use, not occurrence
+				if (ASTUtil.occursFree(n, defPair.a)){
+					ret.addEdge(defPair.b, n);
 				}
 			}
 		}
-		
-		
-		//TODO which to return?
-		return exitSet;
+		return ret;
 	}
 
 }
