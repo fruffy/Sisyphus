09fee07b89c694f7003df4af7315650bdf53fc61
Fabian Ruffy <fruffy>
2017-02-26 18:30:50 -0800

    add new edge type

---
+++ 507/src/datastructures/BackEdge.java
@@ -0,0 +1,16 @@
+package datastructures;
+
+import jgrapht.graph.DefaultEdge;
+
+public class BackEdge extends DefaultEdge {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5936790103330956790L;
+
+	public BackEdge() {
+		super();
+	}
+
+}
