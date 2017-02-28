c40b1cccfdaa326381309f5c97383cb90fa271ca
Fabian Ruffy <fruffy>
2017-02-27 23:14:13 -0800

    Add entry statement (replacing deprecated empty statement)

---
+++ 507/src/datastructures/EntryStmt.java
@@ -0,0 +1,26 @@
+package datastructures;
+
+import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.visitor.GenericVisitor;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+
+public class EntryStmt extends Statement {
+
+    public EntryStmt() {
+        super(null);
+    }
+
+	@Override
+	public <R, A> R accept(GenericVisitor<R, A> v, A arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <A> void accept(VoidVisitor<A> v, A arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
